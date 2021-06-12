880c6a5f4f610220676d168a30c2804ccfecf903
SeungHun6450 <toffg6450@naver.com>
2021-06-12 23:16:11 +0900

    eslint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,3 +1,7 @@
 <template>
-  <img src="~assets/logo.png" alt="HEROPY" />
+<!--eslint의 규칙에 맞게 작성 + 내 입맛에 맞게 수정-->
+  <img 
+    src="~assets/logo.png" 
+    alt="HEROPY" />
+  <div></div>
 </template>
